4f92c2d6d15d8c75ea72483c0189883203283b68
Colin S. Gordon <csgordon@cs.washington.edu>
2012-06-07 16:17:18 -0700

    1759

---
+++ src/subclipse/org.tigris.subversion.subclipse.graph/src/org/tigris/subversion/subclipse/graph/editors/package-info.java
@@ -0,0 +1,5 @@
+@UIPackage
+package org.tigris.subversion.subclipse.graph.editors;
+
+// This must come *AFTER* the package name, oddly enough.
+import guitypes.checkers.quals.*;
---
+++ src/subclipse/org.tigris.subversion.subclipse.graph/src/org/tigris/subversion/subclipse/graph/popup/actions/package-info.java
@@ -0,0 +1,5 @@
+@UIPackage
+package org.tigris.subversion.subclipse.graph.popup.actions;
+
+// This must come *AFTER* the package name, oddly enough.
+import guitypes.checkers.quals.*;
