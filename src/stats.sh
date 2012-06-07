ecbc13ff9d0eef56b00b3b0813041af852bf00b8
Colin S. Gordon <csgordon@cs.washington.edu>
2012-06-07 16:07:12 -0700

    2555

--- src/stats.sh
+++ src/stats.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat log.txt | grep "error:" | cut -f 1 -d ':' | uniq -c | sort
+cat log.txt | grep "error:" | cut -f 1 -d ':' | sort | uniq -c
---
+++ src/subclipse/org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/dialogs/package-info.java
@@ -0,0 +1,5 @@
+@UIPackage
+package org.tigris.subversion.subclipse.ui.dialogs;
+
+// This must come *AFTER* the package name, oddly enough.
+import guitypes.checkers.quals.*;
